47b5d30aa1f84d1d8c0e13b6773d2ebfc6d64f7e
codemattcode <codemattcode@gmail.com>
2017-04-30 00:33:15 +0100

    cl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { routes } from './app.router';
+
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule, 
+    routes
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.router.ts
@@ -0,0 +1,16 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+
+export const router: Routes = [
+  {
+    path: '', redirectTo: 'about', pathMatch: 'full'
+  },
+  {
+    path: 'about', component: AboutComponent
+  }
+];
+
+export const routes: ModuleWithProviders = RouterModule.forRoot(router);
